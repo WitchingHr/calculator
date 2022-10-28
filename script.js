192048e8ca0c82855abc310d384a7398e7202e7e
WitchingHr <matt1494@gmail.com>
2022-10-28 18:08:52 -0400

    Write operator functions

---
+++ script.js
@@ -0,0 +1,32 @@
+// Operator functions
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+// Operate
+
+function operate(x, operator, y) {
+    switch (operator) {
+        case '+' :
+            return add(x, y);
+        case '-' :
+            return subtract(x, y);
+        case '*' :
+            return multiply(x, y);
+        case '/' :
+            return divide(x, y);    
+    }
+}
