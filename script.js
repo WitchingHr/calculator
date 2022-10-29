31dbecc910deab0dfe9ecc0e913225b77228d9cf
WitchingHr <matt1494@gmail.com>
2022-10-28 23:35:19 -0400

    Add operator listener and storer functions

--- script.js
+++ script.js
@@ -43,6 +43,17 @@ function getNumber(e) {
     return value;
 }
 
+// Store value
+
+let storedValue = '';
+
+function storeValue(value) {
+    storedValue = value;
+    console.log(storedValue);
+    // clear screen
+    // reset value
+}
+
 // Populate screen
 
 const screenBottom = document.querySelector('.screen-bottom');
@@ -51,10 +62,32 @@ function populateScreen(num) {
     screenBottom.textContent = num;
 }
 
+// Get operator
+
+let operator = '';
+
+function getOperator(e) {
+    operator = e.target.textContent;
+    console.log(operator);
+    storeValue(value);
+    clearScreen();
+}
+
+// Clear screen
+
+function clearScreen() {
+    value = '';
+    populateScreen();
+}
+
 // Numbers event listeners
 
 const numbers = document.querySelectorAll('.number');
 
-numbers.forEach(number => {
-    number.addEventListener('click', getNumber);
-})
+numbers.forEach(number => number.addEventListener('click', getNumber));
+
+// Operators event listeners
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach(operator => operator.addEventListener('click', getOperator));
