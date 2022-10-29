45e455c01b667176c70813c7334a9ab366398882
WitchingHr <matt1494@gmail.com>
2022-10-29 13:56:17 -0400

    Fix decimal problem for division

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function operate(x, operator, y) {
         case '*' :
             return multiply(x, y);
         case '/' :
-            return divide(x, y);    
+            return divide(x, y).toFixed(2).replace(/[.]00$/, "");  
     }
 }
 
@@ -36,30 +36,21 @@ let value = '';
 function getNumber(e) {
     if (value == '') {
         populateScreen(e.target.textContent);
-        return value = e.target.textContent;
+        return value = Number(e.target.textContent);
     }
     value += e.target.textContent;
     populateScreen(value);
     return value;
 }
 
-// Store value
+// Store value, clear value
 
 let storedValue = '';
 
 function storeValue(value) {
     storedValue = value;
-    console.log(storedValue);
-    // clear screen
-    // reset value
-}
-
-// Populate screen
-
-const screenBottom = document.querySelector('.screen-bottom');
-
-function populateScreen(num) {
-    screenBottom.textContent = num;
+    console.log(storedValue); // remove this
+    value = '';
 }
 
 // Get operator
@@ -68,9 +59,25 @@ let operator = '';
 
 function getOperator(e) {
     operator = e.target.textContent;
-    console.log(operator);
+    console.log(operator); // remove this
     storeValue(value);
     clearScreen();
+}
+
+// Solve
+
+function solve() {
+    value = operate(storedValue, operator, value);
+    populateScreen(value);
+}
+
+
+// Populate screen
+
+const screenBottom = document.querySelector('.screen-bottom');
+
+function populateScreen(num) {
+    screenBottom.textContent = num;
 }
 
 // Clear screen
@@ -91,3 +98,9 @@ numbers.forEach(number => number.addEventListener('click', getNumber));
 const operators = document.querySelectorAll('.operator');
 
 operators.forEach(operator => operator.addEventListener('click', getOperator));
+
+// Equals event listener
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', solve);
